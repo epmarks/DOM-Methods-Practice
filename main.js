dc8d31412db8b46889e2174b582d05f5740fdeaf
Emery <epmarks@ymail.com>
2020-10-23 15:15:39 -0400

    added elements for stretchy goals. Non-functional

--- main.js
+++ main.js
@@ -9,20 +9,67 @@ imageElement.src =
 
 let buttonElement = document.createElement("button");
 buttonElement.id = "removeButton";
-buttonElement.innerText = "Remove";
+buttonElement.innerText = "Remove Main";
+
+let restoreElement = document.createElement("button");
+restoreElement.id = "restoreButton";
+restoreElement.innerText = "Restore Main";
 
 let anchorElement = document.createElement("a");
 anchorElement.class = "link";
 anchorElement.href = "https://www.miniaturemarket.com/";
 anchorElement.innerHTML = "Buy A Board Game!";
 
+let removeInput = document.createElement("input");
+removeInput.id = "removeInput";
+removeInput.placeholder = "Enter a class to remove";
+
+let removeInputButton = document.createElement("button");
+removeInputButton.id = "removeInputButton";
+removeInputButton.innerText = "Remove Class";
+
+let addDivInput = document.createElement("input");
+addDivInput.id = "addInput";
+addDivInput.placeholder = "Enter text to add to the page";
+
+let addTextButton = document.createElement("button");
+addTextButton.id = "addTextButton";
+addTextButton.innerText = "Add Text";
+
 bodyElement.append(buttonElement);
+bodyElement.append(restoreElement);
+bodyElement.append(removeInput);
+bodyElement.append(removeInputButton);
+bodyElement.append(addDivInput);
+bodyElement.append(addTextButton);
 bodyElement.append(mainElement);
 mainElement.append(imageElement);
 mainElement.append(anchorElement);
 
 let remove = document.querySelector("#removeButton");
+let restore = document.querySelector("#restoreButton");
+let removeClass = document.querySelector("#removeInputButton");
+let addText = document.querySelector("#addTextButton");
 
 remove.addEventListener("click", function () {
   mainElement.remove();
 });
+
+restore.addEventListener("click", function () {
+  bodyElement.append(mainElement);
+});
+
+//Below was my mode of thinking when it came to the Remove Button for Stretchy 1.
+
+// removeClass.addEventListener("click", function () {
+//   arr = ["image", "link", "button", "input"];
+//   i = 0;
+//   while (i < arr.length) {
+//     if (removeInput.value === arr[0]) {
+//       imageElement.remove();
+//     } else if (removeInput.value === arr[1]) {
+//       anchorElement.remove();
+//     }
+//     i++;
+//   }
+// });
